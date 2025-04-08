5b74d8d04571a58611edccf634dff5385bedeef2
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-04-08 11:41:28 +0530

    save Add java file in main

---
+++ Add.java
@@ -0,0 +1,11 @@
+package Java1;
+
+public class Add {
+    public static void main(String[] args) {
+     int a = 10;
+    int b= 20;
+    int sum = a + b;
+    System.out.println("Addition = " +sum);
+    }
+    
+}
